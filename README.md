a81a90bb8ab1432041696e1a28085933a0f8c4b1
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-11-19 23:17:48 +0100

    scripts compile then run

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
+# README
+
+This is a Client-Server application built in Java using Sockets.
+
+To compile and run the application, firstly make sure to run these two lines of code:
+
 ```bash
 $ chmod +x client.sh
 $ chmod +x server.sh
+```
 
+While not necessary, this `chmod +x` allows you to...  
+
+```
 $ ./server.sh -c          # to compile
 $ ./server.sh -r          # to execute
 ```
--- client.sh
+++ client.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-  echo "Usage: "
-  echo " $0 [-c|--compile]      lorem ipsum"
-  echo " $0 [-r|--run]          lorem ipsum"
-  exit
+    echo "Usage: "
+    echo " $0 [-c|--compile]      Compile the Client application files"
+    echo " $0 [-r|--run]          Compile and run the Client"
+    exit
 fi
 
 if [ "$1" == -c ] || [ "$1" == --compile ]; then
-  echo "compiling"
-  mkdir -p Client/out/classes
-  javac -cp Client/src Client/src/com/aziflaj/irc/client/*.java -d Client/out/classes
+    echo "compiling"
+    mkdir -p Client/out/classes
+    javac -cp Client/src Client/src/com/aziflaj/irc/client/*.java -d Client/out/classes
 elif [ "$1" == -r ] || [ "$1" == --run ]; then
-  echo "running"
-  java -cp Client/out/classes com.aziflaj.irc.client.Client
+    echo "running"
+    mkdir -p Client/out/classes
+    javac -cp Client/src Client/src/com/aziflaj/irc/client/*.java -d Client/out/classes
+    java -cp Client/out/classes com.aziflaj.irc.client.Client
 else
-  echo "I'm confused, what should I do again?"
-  exit
+    echo "I'm confused, what should I do again?"
+    exit
 fi
